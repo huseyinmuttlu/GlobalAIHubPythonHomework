08e6a339658c70bf6c9988dec28fe9a927a56e99
Hüseyin Mutlu <44866049+huseyinmuttlu@users.noreply.github.com>
2020-12-22 00:13:42 +0300

    Add files via upload

---
+++ GlobalAIHubPyhtonHomework.py
@@ -0,0 +1,9 @@
+value1 = int(input("1. değer giriniz="))
+value2 = int(input("2. değer giriniz="))
+value3 = int(input("3. değer giriniz="))
+value4 = int(input("4. değer giriniz="))
+value5 = int(input("5. değer giriniz="))
+
+
+print("1. değer =",value1," 2. değer = ",value2," 3. değer = ",value3,
+      " 4. değer = ",value4," 5. değer = ",value5)""
