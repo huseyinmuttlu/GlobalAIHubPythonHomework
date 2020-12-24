60362c180071213bbffdbc3ec049dc23bdded668
Hüseyin Mutlu <44866049+huseyinmuttlu@users.noreply.github.com>
2020-12-24 19:29:15 +0300

    Add files via upload

---
+++ GlobalAIHubPyhtonHomeworkDay2.py
@@ -0,0 +1,13 @@
+name = input("First name = ")
+surname = input("Last name = ")
+ages = int(input("Age = "))
+years = int(input("Year{####)= "))
+
+
+
+if ages <= 18:
+    print(name," ",surname," ",ages," ",years)
+    print("You can't go out because it's too dangerous")
+else:
+    print(name," ",surname," ",ages," ",years)
+    print("You can go out to the street") 
